65d426a08e7bbe8cf0267062d543765edfc512d4
v_kaiyfeng <v_kaiyfeng@tencent.com>
2019-10-09 15:49:29 +0800

    deploy

---
+++ precache-manifest.2e399ed8345343ff5a0feaf99cf43518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7bb40d502ec16e8311237673e60d66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e02fca70f7a7c564a16",
+    "url": "/static/css/2.11eb94ec.chunk.css"
+  },
+  {
+    "revision": "d53065bd439d94f380dc",
+    "url": "/static/css/main.e956da35.chunk.css"
+  },
+  {
+    "revision": "9e02fca70f7a7c564a16",
+    "url": "/static/js/2.ed1aa93f.chunk.js"
+  },
+  {
+    "revision": "d53065bd439d94f380dc",
+    "url": "/static/js/main.4cccec08.chunk.js"
+  },
+  {
+    "revision": "a73d4bd78e43dd072e18",
+    "url": "/static/js/runtime-main.3b94bf1f.js"
+  }
+]);
